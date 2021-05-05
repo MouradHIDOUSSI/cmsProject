fedf282b237be40281f869eeaa015a86114ed776
MouradHIDOUSSI <mourad0512@gmail.com>
2021-05-05 20:14:52 +0100

    first commit

--- includes/functions.php
+++ includes/functions.php
@@ -1,52 +1,54 @@
 <?php
-function showAllPosts()
-{
-    global $connection;
-    if (isset($_POST['submit'])) {
-        $search = $_POST['search'];
-        $query = "select * from posts where post_tags LIKE '%$search%' ";
-        $searchQuery = mysqli_query($connection, $query);
+    function showAllPosts()
+    {
+        global $connection;
+        if (isset($_POST['submit'])) {
+            $search = $_POST['search'];
+            $query = "select * from posts where post_tags LIKE '%$search%' ";
+            $searchQuery = mysqli_query($connection, $query);
 
-        if (!$searchQuery) {
-            die("Query Failed" . mysqli_error($connection));
-        }
+            if (!$searchQuery) {
+                die("Query Failed" . mysqli_error($connection));
+            }
 
-        $count = mysqli_num_rows($searchQuery);
-        if ($count == 0) {
-            echo "No result";
-        } else {
-            echo "Some Result";
-            while ($row = mysqli_fetch_assoc($searchQuery)) {
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
-                //html goes here
-    ?>
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"> <?php echo $post_title; ?> </a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author; ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="cmsproject">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
-    <?php  }
+            $count = mysqli_num_rows($searchQuery);
+            if ($count == 0) {
+                echo "No result";
+            } else {
+                echo "Some Result";
+                while ($row = mysqli_fetch_assoc($searchQuery)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    //html goes here
+        ?>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"> <?php echo $post_title; ?> </a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author; ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="cmsproject">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <hr>
+        <?php  }
+            }
         }
     }
-}
+?>
 
+<?php
     function showAllCategoriesSidebar()
     {
         global $connection;
@@ -65,7 +67,6 @@ function showAllPosts()
                 </ul>
             </div>
         </div>
-    <?php
-        
+    <?php  
     }
 ?>
