5298da5f09642f4b20170db656fdacb7b5477cd5
MouradHIDOUSSI <mourad0512@gmail.com>
2021-05-06 17:45:03 +0100

    first commit

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,14 +1,19 @@
 <?php
     if(isset($_POST['create_post'])){
-        $post_title = $_POST['title'];
-        $post_user = $_POST['post_user'];
-        $post_category_id = $_POST['post_category'];
+
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category_id'];
+        $post_author = $_POST['post_author'];
         $post_status = $_POST['post_status'];
         $post_image = $_FILES['image']['name'];
         $post_image_temp = $_FILES['image']['tmp_name'];
         $post_tags = $_POST['post_tags'];
         $post_content = $_POST['post_content'];
         $post_date = date('d-m-y');
+        $post_comment_count = 4;
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
     }
 ?>
 
@@ -16,12 +21,12 @@
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <input type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
        <label for="post_category">Post Category ID</label>
-       <input type="text" class="form-control" name="post_category-id">
+       <input type="text" class="form-control" name="post_category_id">
     </div>
 
     <div class="form-group">
@@ -36,7 +41,7 @@
 
     <div class="form-group">
        <label for="post_image">Post Image</label>
-       <input type="file" name="post_image">
+       <input type="file" name="image">
     </div>
 
     <div class="form-group">
